207c6509d94a1f41f6fe1aea5c4a7bf36f2bb14d
mshan <mshankc@gmail.com>
2024-05-29 23:31:22 +0530

    patch 2

--- lib/components/media_container.dart
+++ lib/components/media_container.dart
@@ -21,59 +21,57 @@ class PhotoContainer extends StatelessWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 15),
-        child: Flexible(
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(bottom: 10),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 15),
-                      child: FaIcon(
-                        FontAwesomeIcons.instagram,
-                        color: AppColors.accentColor,
-                        size: 40,
-                      ),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 15),
+                    child: FaIcon(
+                      FontAwesomeIcons.instagram,
+                      color: AppColors.accentColor,
+                      size: 40,
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 5, right: 100, bottom: 5),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(AppTexts.kInstagramId),
-                          Text('${AppTexts.kInstagramFollowrs} followers')
-                        ],
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 5, right: 100, bottom: 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(AppTexts.kInstagramId),
+                        Text('${AppTexts.kInstagramFollowrs} followers')
+                      ],
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 2),
-                      child: Text(
-                        'More photos',
-                        style: TextStyle(
-                            color: AppColors.accentColor,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 2),
+                    child: Text(
+                      'More photos',
+                      style: TextStyle(
+                          color: AppColors.accentColor,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  )
+                ],
               ),
-              Container(
-                color: Colors.white,
-                width: MediaQuery.of(context).size.width * 1 / 1.3,
-                height: MediaQuery.of(context).size.width * 1 / 5,
-                child: ListView.builder(
-                    itemCount: photoList.length,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 5),
-                        child: PhotoTile(photoAddress: photoList[index]),
-                      );
-                    }),
-              ),
-            ],
-          ),
+            ),
+            Container(
+              color: Colors.white,
+              width: MediaQuery.of(context).size.width * 1 / 1.3,
+              height: MediaQuery.of(context).size.width * 1 / 5,
+              child: ListView.builder(
+                  itemCount: photoList.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: PhotoTile(photoAddress: photoList[index]),
+                    );
+                  }),
+            ),
+          ],
         ),
       ),
     );
--- lib/components/positioned_container.dart
+++ lib/components/positioned_container.dart
@@ -12,144 +12,142 @@ class HomeContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-          color: AppColors.containerColor,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 80,
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.containerColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 80,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Text(AppTexts.kCompanyName,
+                style: AppTextStyle.kCompanyNameTextStyle),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Text(
+              AppTexts.kProductTypeName,
+              style: AppTextStyle.kProductTypeTextStyle,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              child: Text(AppTexts.kCompanyName,
-                  style: AppTextStyle.kCompanyNameTextStyle),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              child: Text(
-                AppTexts.kProductTypeName,
-                style: AppTextStyle.kProductTypeTextStyle,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              child: Row(
-                children: [
-                  const Icon(
-                    Icons.favorite,
-                    color: AppColors.accentColor,
-                  ),
-                  Text(
-                    AppTexts.kUserFavourites,
-                    style: GoogleFonts.darkerGrotesque(
-                        fontSize: 20, fontWeight: FontWeight.w600),
-                  ),
-                  const Divider(
-                    indent: 5,
-                    endIndent: 10,
-                    color: AppColors.dividerColor,
-                  ),
-                  Container(
-                    width: 160,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      gradient: LinearGradient(
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                        colors: [Colors.white, Colors.green.withOpacity(.4)],
-                      ),
-                    ),
-                    child: const Row(
-                      children: [
-                        Icon(
-                          Icons.star_sharp,
-                          color: AppColors.starColor,
-                        ),
-                        Text(AppTexts.kStarText1),
-                        Text(
-                          AppTexts.kStarText2,
-                          style: TextStyle(
-                              color: AppColors.starColor,
-                              fontWeight: FontWeight.w500),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: ExpandableText(
-                  AppTexts.kAppDescription,
-                  expandText: 'read more',
-                  collapseText: 'show less',
-                  linkColor: AppColors.accentColor,
-                  linkStyle: const TextStyle(fontWeight: FontWeight.w500),
-                  maxLines: 2,
-                  style: AppTextStyle.kExpandDescriptionTextStyle,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.favorite,
+                  color: AppColors.accentColor,
                 ),
-              ),
-            ),
-            const Divider(
-              color: AppColors.dividerColor,
-              indent: 40,
-              endIndent: 40,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: Row(
+                Text(
+                  AppTexts.kUserFavourites,
+                  style: GoogleFonts.darkerGrotesque(
+                      fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                const Divider(
+                  indent: 5,
+                  endIndent: 10,
+                  color: AppColors.dividerColor,
+                ),
+                Container(
+                  width: 160,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [Colors.white, Colors.green.withOpacity(.4)],
+                    ),
+                  ),
+                  child: const Row(
                     children: [
-                      const Icon(
-                        Icons.location_on_rounded,
-                        color: AppColors.accentColor,
+                      Icon(
+                        Icons.star_sharp,
+                        color: AppColors.starColor,
                       ),
-                      const SizedBox(
-                        width: 10,
-                      ),
+                      Text(AppTexts.kStarText1),
                       Text(
-                        'Get delivery location',
-                        style: GoogleFonts.darkerGrotesque(
-                            fontWeight: FontWeight.w600, fontSize: 18),
+                        AppTexts.kStarText2,
+                        style: TextStyle(
+                            color: AppColors.starColor,
+                            fontWeight: FontWeight.w500),
                       ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      const Icon(
-                        Icons.arrow_forward_ios,
-                        color: AppColors.accentColor,
-                        size: 14,
-                      )
                     ],
                   ),
                 ),
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ExpandableText(
+                AppTexts.kAppDescription,
+                expandText: 'read more',
+                collapseText: 'show less',
+                linkColor: AppColors.accentColor,
+                linkStyle: const TextStyle(fontWeight: FontWeight.w500),
+                maxLines: 2,
+                style: AppTextStyle.kExpandDescriptionTextStyle,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 15.0,
-              ),
-              child: Text(
-                'Delivery fee will apply',
-                style: GoogleFonts.darkerGrotesque(
-                    color: AppColors.textColor2, fontSize: 16),
+          ),
+          const Divider(
+            color: AppColors.dividerColor,
+            indent: 40,
+            endIndent: 40,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.location_on_rounded,
+                      color: AppColors.accentColor,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Get delivery location',
+                      style: GoogleFonts.darkerGrotesque(
+                          fontWeight: FontWeight.w600, fontSize: 18),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Icon(
+                      Icons.arrow_forward_ios,
+                      color: AppColors.accentColor,
+                      size: 14,
+                    )
+                  ],
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 15.0,
+            ),
+            child: Text(
+              'Delivery fee will apply',
+              style: GoogleFonts.darkerGrotesque(
+                  color: AppColors.textColor2, fontSize: 16),
+            ),
+          ),
+        ],
       ),
     );
   }
